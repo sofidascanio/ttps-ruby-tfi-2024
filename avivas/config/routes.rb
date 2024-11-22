3f8c01e789151c49846e054147de2b9d6dc02a2d
Sofía D´Ascanio <sofiadascanio1@gmail.com>
2024-11-22 16:37:18 -0300

    agrego modelos producto, categoria, venta, usuario

--- avivas/config/routes.rb
+++ avivas/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :sales
+  resources :categories
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- avivas/db/schema.rb
+++ avivas/db/schema.rb
@@ -10,5 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_22_193438) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.integer "stock"
+    t.string "color"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.datetime "sale_date"
+    t.float "sale_price"
+    t.string "client"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "telephone"
+    t.string "password_digest"
+    t.datetime "entered_at"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
 end
