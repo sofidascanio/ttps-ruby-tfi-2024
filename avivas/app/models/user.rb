6cb265419f51c0568402a1313fbfd2fa46aba2e7
Sofía D´Ascanio <sofiadascanio1@gmail.com>
2024-11-22 17:38:12 -0300

    agrego bcrypt para las contraseñas

--- avivas/app/models/user.rb
+++ avivas/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :telephone, presence: true
+    validates :role, presence: true
 end
