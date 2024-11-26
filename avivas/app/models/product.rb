51ca573e8204cf101d5bce51f6b7afb9edc216de
Sofía D´Ascanio <sofiadascanio1@gmail.com>
2024-11-26 17:04:24 -0300

    agrego gema color para producto

--- avivas/app/models/product.rb
+++ avivas/app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    # Valida que el color esté en formato hexadecimal
+    validates :color, presence: true, format: { with: /\A#[0-9a-fA-F]{6}\z/, message: "debe ser un código hexadecimal válido" }
+
+    # Método para obtener un objeto de color a partir del código hexadecimal almacenado
+    def color_object
+        Color::RGB.from_hex(self.color) # Convierte el código hexadecimal a un objeto RGB
+    end
 end
