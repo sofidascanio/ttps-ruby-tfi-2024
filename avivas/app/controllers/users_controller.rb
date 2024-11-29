6299a958d893898b2dc5edd916ad6ddc8c5018ed
Sofía D´Ascanio <sofiadascanio1@gmail.com>
2024-11-29 19:31:38 -0300

    agrego controlador de usuarios y vistas

---
+++ avivas/app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+    before_action :authenticate_user! 
+    before_action :set_user, only: %i[ show edit update destroy ]
+
+    def index
+        @users = User.all
+    end
+
+    def show
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to users_path, notice: "Usuario creado exitosamente."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to @user, notice: "Usuario actualizado exitosamente."
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user.destroy
+        redirect_to users_path, notice: "Usuario eliminado exitosamente."
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :username, :telephone, :role)
+    end
+end
+  
