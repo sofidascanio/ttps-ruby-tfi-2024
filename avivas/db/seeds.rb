c4d6988d3d568b1607577b7c92d1080996219aa8
Sofía D´Ascanio <sofiadascanio1@gmail.com>
2024-12-05 19:23:12 -0300

    arreglo js, cambio seeds a español

--- avivas/db/seeds.rb
+++ avivas/db/seeds.rb
@@ -8,6 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+Faker::Config.locale = :es
+
 User.create!(
     username: "sofiad",
     email: "sofia@gmail.com",
@@ -19,58 +21,62 @@ User.create!(
 
 15.times do
     begin
-      User.create!(
-        username: Faker::Internet.unique.username(specifier: 5..12),
-        email: Faker::Internet.unique.email,
-        telephone: Faker::Number.leading_zero_number(digits: rand(7..15)),
-        password: Faker::Internet.password(min_length: 6),
-        role: rand(0..2),
-        entered_at: Faker::Date.backward(days: 365 * 5) 
-      )
+        User.create!(
+            username: Faker::Internet.unique.username(specifier: 5..12),
+            email: Faker::Internet.unique.email,
+            telephone: rand(10**6..10**14).to_s, 
+            password: Faker::Internet.password(min_length: 6),
+            role: rand(0..2),
+            entered_at: Faker::Date.backward(days: 365 * 5)
+        )
     rescue ActiveRecord::RecordInvalid => e
-      puts "Error al crear usuario: #{e.message}"
+        puts "Error al crear usuario: #{e.message}"
     end
 end
 
-sizes = ["Small", "Medium", "Large", "Extra Large", nil] 
-
+sizes = ["Pequeño", "Mediano", "Grande", "Extra Grande", nil]
 
 25.times do
     Product.create!(
-      name: Faker::Commerce.unique.product_name,  
-      description: Faker::Lorem.sentence(word_count: 15), 
-      price: Faker::Commerce.price(range: 1.0..1000.0, as_string: false),
-      stock: rand(0..100), 
-      color: Faker::Color.hex_color,
-      size: sizes.sample
+        name: "#{Faker::Commerce.material} #{Faker::Commerce.product_name}",
+        description: Faker::Lorem.sentence(word_count: 15),
+        price: Faker::Commerce.price(range: 1.0..1000.0, as_string: false),
+        stock: rand(0..100),
+        color: Faker::Color.color_name,
+        size: sizes.sample
     )
 end
-  
 
 50.times do
     employee = User.order('RANDOM()').first
 
     sale = Sale.create!(
-        client: Faker::Name.name, 
-        user: employee           
+        client: Faker::Name.name,
+        user: employee
     )
 
     products = Product.order('RANDOM()').limit(rand(1..5))
 
+    total_price = 0
     products.each do |product|
         ProductSale.create!(
-        sale: sale,
-        product: product
+            sale: sale,
+            product: product,
+            price: product.price,
+            quantity: rand(1..15) 
         )
+        total_price = total_price + product.price
     end
+
+    sale.sale_price = total_price
 end
 
 15.times do
     category = Category.create!(
-      name: Faker::Commerce.department(max: 1, fixed_amount: true) 
+        name: Faker::Commerce.department(max: 1, fixed_amount: true) 
     )
-  
+
     products = Product.order('RANDOM()').limit(5)
-  
+
     category.products << products
 end
