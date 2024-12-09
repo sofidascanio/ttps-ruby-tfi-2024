3f2f041b0957f92e93679d4ff10c84c49993da73
Sofía D´Ascanio <sofiadascanio1@gmail.com>
2024-12-09 16:04:57 -0300

    agrego chequeo de permisos a los html

--- avivas/app/controllers/categories_controller.rb
+++ avivas/app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.order(created_at: :desc).page(params[:page])
+    @categories = Category.order(created_at: :desc).page(params[:page]).per(20)
   end
 
   # GET /categories/1 or /categories/1.json
--- avivas/config/initializers/kaminari_config.rb
+++ avivas/config/initializers/kaminari_config.rb
@@ -2,7 +2,7 @@
 
 Kaminari.configure do |config|
   config.default_per_page = 10
-  config.max_per_page = 12
+  config.max_per_page = 25
   config.window = 2
   # config.outer_window = 0
   # config.left = 0
